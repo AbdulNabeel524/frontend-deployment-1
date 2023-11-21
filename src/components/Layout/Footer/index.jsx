4cfdeceb5e47ccd49b3c44790453e8101f169fbe
AbdulNabeel524 <abdulnabeelmaistry@gmail.com>
2023-11-21 14:36:27 +0530

    first commit

---
+++ src/components/Layout/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer className="general_footer">
+      <span className="copyright">Citizen Journalism, {new Date().getFullYear()}. ALL RIGHTS RECEIVED</span>
+      
+    </footer>
+  )
+}
+
+export default Footer;
